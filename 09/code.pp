53e89beffe5341fc4fc9892c315592fa0a8ec83e
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2020-09-30 22:10:41 +0530

    Create code.pp

---
+++ 09/code.pp
@@ -0,0 +1,18 @@
+node default { }
+node 'stapp01.com', 'stapp02.com', 'stapp03.com' {
+     include ntpconfig
+}
+     
+   class ntpconfig {
+     package { "ntp":
+       ensure => present,
+     }
+     file { "/etc/ntp.conf":
+        ensure => present,
+     }
+     file_line { "ntp.conf":
+        ensure   => present,
+        path     => '/etc/ntp.conf',
+        line     => 'server 1.south-america.pool.ntp.org',
+     }
+}

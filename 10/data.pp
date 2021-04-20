bdecced91756b51dc8a032eb52a7a0b724cef70a
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-20 21:33:16 +0530

    Node details

---
+++ 10/data.pp
@@ -0,0 +1,13 @@
+node default {}
+
+node "stapp01" {
+              include multi_package_node1
+}
+
+node "stapp02" {
+              include multi_package_node2
+}
+
+node "stapp03" {
+              include multi_package_node3
+}
